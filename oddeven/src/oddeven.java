0e3ab79876ae4260ce9d15f5dedb2760b2eb56dd
hp <hp@DESKTOP-9K6ATCB>
2022-05-23 20:15:03 +0530

    array

---
+++ oddeven/src/oddeven.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class oddeven {
+
+	public static void main(String[] args) {
+	
+			
+						Scanner reader = new Scanner(System.in);
+						System.out.print("enter a number:");
+						int n = reader.nextInt();
+						if(n%2 == 0)
+							System.out.println(n + " is even");
+						else
+							System.out.println(n + "is odd");
+	}
+
+}
+
