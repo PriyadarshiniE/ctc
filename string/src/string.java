2a2ba295407c29db0b8d5b3afe45fdf2bded8c36
hp <hp@DESKTOP-9K6ATCB>
2022-05-25 17:03:24 +0530

    String

---
+++ string/src/string.java
@@ -0,0 +1,13 @@
+
+public class string {
+			public static void main(String args[]){  
+			String s1="hello";  
+			String s2="java";  
+			String s=s1+s2;
+			System.out.println("string length is: "+s.length());
+			
+			 
+
+	}
+
+}
